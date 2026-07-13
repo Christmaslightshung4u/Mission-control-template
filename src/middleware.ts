d55e27359241a0427ad275e6e3b1bc99832bf7f2
Christmaslightshung4u <mark@christmaslightshung4u.com>
2026-07-12 22:10:49 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,4 +2,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const config = { matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'], }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
